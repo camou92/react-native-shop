fe1ca80744200c42d3143a83cff8edf82e98090a
camou92 <cmohamed992@gmail.com>
2023-03-31 23:57:52 +0000

    product details screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ProductsScreen from './src/screens/ProductsScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductsScreen />
+      {/* <ProductsScreen /> */}
+      <ProductDetailsScreen />
       <StatusBar style="auto" />
     </View>
   );
